c465f0c69715a9d86f956ce75912d48e95d16c91
al-wrifi <alwrifisoft@gmail.com>
2025-01-20 08:38:24 +0300

    @AL-WRIFI create : main category controller and blade

---
+++ app/Http/Controllers/Admin/Categories/CategoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Categories;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.category.index', compact('categories'));
+    }
+
+
+    public function create(Request $request) {
+        return view('admin.categories.category.create');
+    }
+
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'image' => 'required|image|mimes:jpg,png,jpeg,gif',
+        ]);
+
+        $data = $request->only(['name','description']);
+
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('images', 'public');
+            $url = Storage::url($path);
+            $data['image'] = $url;
+        }
+
+        $category = Category::create(array_merge($data, ['slug' => Str::slug($data['name'])]));
+
+        return redirect()->route('categories.index');
+    }
+
+    public function edit(Category $category) {
+        return view('admin.categories.category.edit', compact('category'));
+    }
+    
+    public function update(Request $request, Category $category) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'image' => 'nullable|image|mimes:jpg,png,jpeg,gif',
+        ]);
+    
+        $data = $request->only(['name', 'description']);
+    
+        // Update the image if a new one is uploaded
+        if ($request->hasFile('image')) {
+            // Delete the old image if it exists
+            if ($category->image) {
+                $oldImagePath = str_replace('/storage', 'public', $category->image);
+                Storage::delete($oldImagePath);
+            }
+    
+            // Store the new image
+            $path = $request->file('image')->store('images', 'public');
+            $url = Storage::url($path);
+            $data['image'] = $url;
+        }
+    
+        // Update the category
+        $category->update(array_merge($data, ['slug' => Str::slug($data['name'])]));
+    
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully.');
+    }
+}
