54055c057d2743e0a524435709cbde82d1c48af1
al-wrifi <alwrifisoft@gmail.com>
2025-01-22 02:51:08 +0300

    @AL-WRIFI create : products & units migration table

---
+++ database/migrations/2025_01_19_231300_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->float('price');
+            $table->float('sale_price');
+            $table->foreignId('sub_category_id')->constrained('sub_categories')->onDelete('cascade');
+            $table->string('image_url');
+            $table->text('summary')->nullable();
+            $table->text('description')->nullable();
+            $table->foreignId('unit_id')->constrained('units')->onDelete('cascade');
+            $table->float('quantity');
+            $table->boolean('stock')->default(1);
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
