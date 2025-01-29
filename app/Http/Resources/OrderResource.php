6ea163c19e8e495062fe23ad17614a68835fc64c
al-wrifi <alwrifisoft@gmail.com>
2025-01-29 06:38:33 +0300

    @AL-WRIFI create : OrderRequest & OrderResource

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'subtotal' => $this->subtotal,
+            'discount_amount' => $this->discount_amount,
+            'delivery_amount' => $this->delivery_amount,
+            'total_amount' => $this->total_amount,
+            'status' => $this->status,
+            'shipping_address' => $this->shipping_address,
+            'payment_method' => $this->payment_method,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+                'email' => $this->user->email,
+            ],
+            'items' => $this->orderItems->map(function ($item) {
+                return [
+                    'product_id' => $item->product_id,
+                    'product_name' => $item->product_name,
+                    'product_price' => $item->product_price,
+                    'quantity' => $item->quantity,
+                ];
+            }),
+        ];
+    }
+}
