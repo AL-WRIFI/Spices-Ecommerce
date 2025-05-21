cc27e06f8e10d3e37cc492a153084d68c87b7277
al-wrifi <alwrifisoft@gmail.com>
2025-05-21 15:55:22 +0300

    @AL-WRIFI update : database seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,73 +4,50 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
-
+use Faker\Factory as Faker;
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
+        $faker = Faker::create();
+
         $categories = [
             [
-                'name' => 'بهارات مطحونة',
-                'image' => 'https://images.pexels.com/photos/5945749/pexels-photo-5945749.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'مجموعة متنوعة من البهارات المطحونة الطازجة.',
+                'name' => 'Electronics',
+                'slug' => 'electronics',
+                'image' => 'https://picsum.photos/400/300?random=1',
+                'description' => $faker->sentence(6),
             ],
             [
-                'name' => 'أعشاب مجففة',
-                'image' => 'https://images.pexels.com/photos/139259/pexels-photo-139259.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'أعشاب عطرية مجففة بعناية للحفاظ على نكهتها.',
+                'name' => 'Clothing',
+                'slug' => 'clothing',
+                'image' => 'https://picsum.photos/400/300?random=2',
+                'description' => $faker->sentence(6),
             ],
             [
-                'name' => 'بهارات حب كاملة',
-                'image' => 'https://images.pexels.com/photos/262969/pexels-photo-262969.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'حبوب بهارات كاملة لطحنها طازجة أو استخدامها كما هي.',
+                'name' => 'Home & Kitchen',
+                'slug' => 'home-kitchen',
+                'image' => 'https://picsum.photos/400/300?random=3',
+                'description' => $faker->sentence(6),
             ],
             [
-                'name' => 'فلفل وبابريكا',
-                'image' => 'https://images.pexels.com/photos/1367243/pexels-photo-1367243.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'أنواع مختلفة من الفلفل الحار والحلو والبابريكا.',
+                'name' => 'Books',
+                'slug' => 'books',
+                'image' => 'https://picsum.photos/400/300?random=4',
+                'description' => $faker->sentence(6),
             ],
             [
-                'name' => 'بذور صحية',
-                'image' => 'https://images.pexels.com/photos/8951404/pexels-photo-8951404.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'مجموعة من البذور الغنية بالفوائد الصحية.',
-            ],
-            [
-                'name' => 'خلطات بهارات جاهزة',
-                'image' => 'https://img.freepik.com/free-photo/top-view-different-condiments-with-salt-red-pepper-black-pepper-dark-desk_140725-65456.jpg?w=740',
-                'description' => 'خلطات مبتكرة وجاهزة للاستخدام لأطباق متنوعة.',
-            ],
-            [
-                'name' => 'زعفران وكركم', 
-                'image' => 'https://images.pexels.com/photos/60088/saffron-crocus-threads-spice-60088.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'زعفران فاخر وكركم عالي الجودة.',
-            ],
-            [
-                'name' => 'شاي وأعشاب للشرب',
-                'image' => 'https://images.pexels.com/photos/230491/pexels-photo-230491.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'أوراق شاي وأعشاب مختارة لتحضير مشروبات لذيذة.',
-            ],
-            [
-                'name' => 'محليات طبيعية',
-                'image' => 'https://images.pexels.com/photos/209304/pexels-photo-209304.jpeg?auto=compress&cs=tinysrgb&w=600',
-                'description' => 'بدائل صحية للسكر مثل العسل ودبس التمر.',
-            ],
-            [
-                'name' => 'منتجات الملح والفانيليا',
-                'image' => 'https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&w=600', // صورة ملح، يمكن إضافة صورة فانيليا إذا دمجت
-                'description' => 'أنواع مختلفة من الملح الطبيعي وأعواد وقرون الفانيليا.',
+                'name' => 'Sports',
+                'slug' => 'sports',
+                'image' => 'https://picsum.photos/400/300?random=5',
+                'description' => $faker->sentence(6),
             ],
         ];
 
-        foreach ($categories as $categoryData) {
-            Category::create([
-                'name' => $categoryData['name'],
-                'slug' => Str::slug($categoryData['name']),
-                'image' => $categoryData['image'],
-                'description' => $categoryData['description'],
-            ]);
+        foreach ($categories as $category) {
+            Category::create($category);
         }
-        $this->command->info(count($categories) . ' categories seeded successfully.');
+
+        $this->command->info('5 categories seeded successfully.');
     }
 }
--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -5,41 +5,32 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class SubCategorySeeder extends Seeder
 {
     public function run(): void
     {
-        $subCategories = [
-            ['category_name' => 'بهارات مطحونة', 'name' => 'كمون ناعم', 'image' => 'https://images.pexels.com/photos/13202005/pexels-photo-13202005.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'كمون مطحون بجودة عالية.'],
-            ['category_name' => 'بهارات مطحونة', 'name' => 'كزبرة ناعمة', 'image' => 'https://images.pexels.com/photos/6792330/pexels-photo-6792330.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'كزبرة مطحونة طازجة.'],
-            ['category_name' => 'أعشاب مجففة', 'name' => 'زعتر بري مجفف', 'image' => 'https://images.pexels.com/photos/760281/pexels-photo-760281.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'زعتر بري مجفف للطهي والشاي.'],
-            ['category_name' => 'أعشاب مجففة', 'name' => 'نعناع مجفف', 'image' => 'https://images.pexels.com/photos/10199870/pexels-photo-10199870.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'نعناع مجفف منعش.'],
-            ['category_name' => 'بهارات حب كاملة', 'name' => 'فلفل أسود حب', 'image' => 'https://images.pexels.com/photos/4198157/pexels-photo-4198157.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'حبوب فلفل أسود كاملة.'],
-            ['category_name' => 'بهارات حب كاملة', 'name' => 'هيل أخضر حب', 'image' => 'https://images.pexels.com/photos/6111614/pexels-photo-6111614.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'هيل حب فاخر للقهوة والحلويات.'],
-            ['category_name' => 'فلفل وبابريكا', 'name' => 'بابريكا حلوة', 'image' => 'https://images.pexels.com/photos/8472456/pexels-photo-8472456.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'مسحوق بابريكا حلوة لإضافة لون ونكهة.'],
-            ['category_name' => 'بذور صحية', 'name' => 'بذور الشيا', 'image' => 'https://images.pexels.com/photos/4061397/pexels-photo-4061397.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'بذور الشيا العضوية الغنية بالفوائد.'],
-            ['category_name' => 'خلطات بهارات جاهزة', 'name' => 'بهارات الكبسة', 'image' => 'https://img.freepik.com/free-photo/top-view-different-condiments-with-salt-red-pepper-black-pepper-dark-desk_140725-65456.jpg?w=740', 'description' => 'خلطة بهارات خاصة للكبسة السعودية.'],
-            ['category_name' => 'زعفران وكركم', 'name' => 'خيوط الزعفران الأصلي', 'image' => 'https://images.pexels.com/photos/713300/pexels-photo-713300.jpeg?auto=compress&cs=tinysrgb&w=600', 'description' => 'زعفران نقيل فاخر درجة أولى.'],
-        ];
+        $categories = Category::all();
 
-        $count = 0;
-        foreach ($subCategories as $subCategoryData) {
-            $category = Category::where('name', $subCategoryData['category_name'])->first();
-            if ($category) {
+        if ($categories->isEmpty()) {
+            $this->command->error('No categories found. Please seed categories first.');
+            return;
+        }
+        $faker = Faker::create();
+
+        foreach ($categories as $category) {
+            for ($i = 1; $i <= 3; $i++) {
                 SubCategory::create([
-                    'name' => $subCategoryData['name'],
-                    'slug' => Str::slug($subCategoryData['name']),
-                    'image' => $subCategoryData['image'],
-                    'description' => $subCategoryData['description'],
+                    'name' => $category->name . ' SubCategory ' . $i,
+                    'slug' => $category->slug . '-subcategory-' . $i,
+                    'image' => 'https://picsum.photos/400/300?random=' . rand(10, 100),
+                    'description' => $faker->sentence(6),
                     'category_id' => $category->id,
                 ]);
-                $count++;
-            } else {
-                $this->command->warn("Category '{$subCategoryData['category_name']}' not found for subcategory '{$subCategoryData['name']}'. Skipping.");
             }
         }
-        $this->command->info($count . ' subcategories seeded successfully.');
+
+        $this->command->info('Subcategories seeded successfully.');
     }
 }
