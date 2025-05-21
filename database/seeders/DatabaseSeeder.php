c5509513ec2db2cda067f3c3eef37e329b1f5123
al-wrifi <alwrifisoft@gmail.com>
2025-05-21 15:58:16 +0300

    @AL-WRIFI update : user facrory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
 {
   public function run(): void
   {
-      User::factory(3)->create();
+      // User::factory(3)->create();
       $this->call([
         CountriesTableSeeder::class,
         RegionsTableSeeder::class,
